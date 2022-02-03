164bccb519b0a36b0e26421672451df94aed5a22
Roberto Gomes <rjrg.snf20@uea.edu.br>
2022-02-03 14:45:59 -0400

    New Get request
    Now the get request give u all the posts already created.

--- Atividade_001-Treinando_FULL_STACK_DEVELOPMENT/server/routes/Posts.js
+++ Atividade_001-Treinando_FULL_STACK_DEVELOPMENT/server/routes/Posts.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { Posts } = require('../models');
 
-router.get("/", (req, res) => {
-    res.send("Hello word")
+router.get("/", async (req, res) => {
+    const listOfPosts = await Posts.findAll();
+    res.json(listOfPosts);
 });
 
 router.post("/", async (req, res) => {
