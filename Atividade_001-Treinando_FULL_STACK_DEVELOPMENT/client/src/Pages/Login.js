9df82d9585db03589970ecb4ac9ffeca479f3bf7
Roberto Gomes <rjrg.snf20@uea.edu.br>
2022-02-05 17:54:13 -0400

    EP10

--- Atividade_001-Treinando_FULL_STACK_DEVELOPMENT/client/src/Pages/Login.js
+++ Atividade_001-Treinando_FULL_STACK_DEVELOPMENT/client/src/Pages/Login.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from "../helpers/AuthContext";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const {setAuthState} = useContext(AuthContext)
 
-  const login = () => {
+    let history = useHistory();
+
+    const login = () => {
     const data = { username: username, password: password };
     axios.post("http://localhost:3001/auth/login", data).then((response) => {
       if(response.data.error) alert(response.data.error);
-      else sessionStorage.setItem("accessToken", response.data);
+      else{
+          localStorage.setItem("accessToken", response.data);
+          setAuthState(true);
+          history.push("/");
+      }
     });
   };
   return (
