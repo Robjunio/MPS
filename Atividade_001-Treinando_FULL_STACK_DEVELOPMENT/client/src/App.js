666b4e645fb0ede469da98c3451e3d127d02c5a6
Roberto Gomes <rjrg.snf20@uea.edu.br>
2022-02-05 20:16:15 -0400

    EP11

--- Atividade_001-Treinando_FULL_STACK_DEVELOPMENT/client/src/App.js
+++ Atividade_001-Treinando_FULL_STACK_DEVELOPMENT/client/src/App.js
@@ -39,42 +39,40 @@ function App() {
 
   const logout = () => {
     localStorage.removeItem("accessToken");
-    setAuthState({
-      username: "", 
-      id: 0, 
-      status: false,
-    });
+    setAuthState({...authState, status: false });
   };
 
-  return <div className="App">
-    <AuthContext.Provider value={{authState, setAuthState}}>
-      <Router>
-        <div className="navbar">
-          <Link to="/"> Home </Link>
-          <Link to="/createpost"> Create A Post </Link>
-          {!authState.status ? (
-          <>
-            <Link to="/login"> Login </Link>
-            <Link to="/registration"> registration </Link>
-          </>
-          ) : (
-            <button onClick={logout}> Logout </button>
-          )
-        }
-
-        <h1>{authState.username}</h1>
-      
-        </div>
-        <Switch>
-          <Route path = "/" exact component = {Home} />
-          <Route path = "/createpost" exact component = {CreatePost} />
-          <Route path = "/post/:id" exact component = {Post} />
-          <Route path = "/login" exact component = {Login} />
-          <Route path = "/Registration" exact component = {Registration} />
-        </Switch>
-      </Router>
-    </AuthContext.Provider>
-  </div>;
+  return (
+    <div className="App">
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <Router>
+          <div className="navbar">
+            <div className="links">
+              <Link to="/"> Home Page</Link>
+              <Link to="/createpost"> Create A Post</Link>
+              {!authState.status && (
+                <>
+                  <Link to="/login"> Login</Link>
+                  <Link to="/registration"> Registration</Link>
+                </>
+              )}
+            </div>
+            <div className="loggedInContainer">
+              <h1>{authState.username} </h1>
+              {authState.status && <button onClick={logout}> Logout</button>}
+            </div>
+          </div>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/createpost" exact component={CreatePost} />
+            <Route path="/post/:id" exact component={Post} />
+            <Route path="/registration" exact component={Registration} />
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </Router>
+      </AuthContext.Provider>
+    </div>
+  );
 }
 
 export default App;
